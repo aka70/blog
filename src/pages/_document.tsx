28e7ce5fea63d6794533512f874df2952876412a
aka <akatsukisatoshi@gmail.com>
2022-05-08 06:14:20 +0900

    ファビコンを設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="aka's blog" />
+        <link rel="icon" type="image/svg" sizes="32x32" href="/favicon.svg" />
       </Head>
       <body>
         <Main />
